2b73a887222e0e69fa903383e900f595a121ecfd
DavidSongzw <szw712@gmail.com>
2019-12-05 12:58:05 +0800

    Site updated: 2019-12-05 12:58:05

--- js/temp.js
+++ js/temp.js
@@ -1,43 +1,6 @@
-function sqrtBisection(n) {
-  if (n === 0 || n === 1) return n
+// 升序排列的循环有序数组
+const list = [4, 5, 6, 7, 8, 1, 2, 3]
+function binarySearchCircle(list = [], value) {
   let low = 0
-  let high = n < 1 ? 1 : n
-//   const E = Number.EPSILON // 接受的最小误差范围
-  const E = 0.000001 //指定精度
-  while (true) {
-    let middle = (low + high) / 2
-    if (Math.abs(middle * middle - n) <= e) { return math.floor(middle) } console.log(middle) if (middle * middle - n> E) {
-      high = middle
-    } else {
-      low = middle
-    }
-  }
-}
-// function sqrtBisection(n) {
-//     if (isNaN(n)) return NaN;
-//     if (n === 0 || n === 1) return n;
-//     var low = 0,
-//         high = n,
-//         pivot = (low + high) / 2,
-//         lastPivot = pivot;
-//     // do while 保证执行一次
-//     do {
-//         if (Math.pow(pivot, 2) > n) {
-//             high = pivot;
-//         } else if (Math.pow(pivot, 2) < n) {
-//             low = pivot;
-//         } else {
-//             return pivot;
-//         }
-//         lastPivot = pivot;
-//         pivot = (low + high) / 2;
-//     }
-//     // 2018-04-25 22:08 更新
-//     // 使用Number.EPSILON表示能够接受的最小误差范围
-//     while (Math.abs(pivot - lastPivot) >= Number.EPSILON)
-
-//     return pivot;
-// }
-console.log('sqrtBisection', Math.sqrt(8))
-console.log('sqrtBisection', sqrtBisection(8))
-</=>
+  let high = list.length - 1
+  while (low <= high) { const middle="Math.floor(high" + (low - 2) 循环有序数组被切割后，会分为有序数组和循环有序数组 if (list[middle]="==" value) return } (list[low] <="list[middle])" 左侧有序>= value && list[low] <= 1 value) { high="middle" - } else low="middle" + 右侧有序 if (list[high]>= value && list[middle] </=></=>
