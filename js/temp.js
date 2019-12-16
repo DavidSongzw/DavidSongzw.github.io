f8f9d56d89e11fff46f07320d503afa3316b19ca
DavidSongzw <szw712@gmail.com>
2019-12-16 16:54:09 +0800

    Site updated: 2019-12-16 16:54:08

--- js/temp.js
+++
@@ -1,6 +0,0 @@
-// 升序排列的循环有序数组
-const list = [4, 5, 6, 7, 8, 1, 2, 3]
-function binarySearchCircle(list = [], value) {
-  let low = 0
-  let high = list.length - 1
-  while (low <= high) { const middle="Math.floor(high" + (low - 2) 循环有序数组被切割后，会分为有序数组和循环有序数组 if (list[middle]="==" value) return } (list[low] <="list[middle])" 左侧有序>= value && list[low] <= 1 value) { high="middle" - } else low="middle" + 右侧有序 if (list[high]>= value && list[middle] </=></=>
