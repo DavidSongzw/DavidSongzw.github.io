bf802224a2db2245cce911bbaf32fd843059fa79
宋振威 <zhenwei.song@kuwo.cn>
2020-05-20 18:58:24 +0800

    Site updated: 2020-05-20 18:58:23

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,12 +1,10 @@
-unction find(index, cWeight, items, num, weight) {
-//   // cWeight==w表示装满了;
-//   // index==num表示已经考察完所有的物品
-//   if (cWeight == weight || index == num) {
-//     // 更新最大重量
-//     if (cWeight > maxW) maxW = cWeight
-//     return
-//   }
-//   // 不装进去
-//   find(index + 1, cWeight, items, num, weight)
-//   // 装进去 但是判断下是不是超过了
-//   if (cWeight + items[index] 
+------insert item------------')
+// LList.insert(1, 'qian') // 首元素后插入
+// LList.insert(5, 'zhou') // 尾元素后插入
+// LList.display() // chen -> qian -> curry -> sang -> zhao -> zhou
+// console.log('-------------delete item------------')
+// LList.delete(1)
+// LList.delete(0)
+// LList.display() // curry -> sang -> zhao -> zhou
+// console.log('-------------find by index------------')
+// console.log(LList.find(0))
